e2832de43bbb269da7c393b3ace9d29ed1683599
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-04 03:26:35 +0300

    Snake 0-1
    Made the snake move !
    made the snakeMove function, and the changeDirection functions.

--- snakeGame/app.js
+++ snakeGame/app.js
@@ -34,7 +34,7 @@ function gameStart() {
   createFood();
   drawFood();
   nextTick();
-};
+}
 function nextTick() {
   if (running) {
     setTimeout(() => {
@@ -45,8 +45,7 @@ function nextTick() {
       checkGameOver();
       nextTick();
     }, 75);
-  }
-  else{
+  } else {
     displayGameOver();
   }
 }
@@ -68,17 +67,57 @@ function drawFood() {
   ctx.fillStyle = foodColor;
   ctx.fillRect(foodX, foodY, unitSize, unitSize);
 }
-function moveSnake() {}
+function moveSnake() {
+  const head = { x: snake[0].x + xVelocity, y: snake[0].y + yVelocity };
+  snake.unshift(head);
+  // if food is eaten
+  if (snake[0].x == foodX && snake[0].y == foodY) {
+    score += 1;
+    scoreText.textContent = score;
+    createFood();
+  } else {
+    snake.pop();
+  }
+}
 function drawSnake() {
   ctx.fillStyle = snakeColor;
   ctx.strokeStyle = snakeBorder;
-  snake.forEach(snakePart => {
+  snake.forEach((snakePart) => {
     ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
     ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
-
-  })
+  });
 }
-function changeDirection() {}
+function changeDirection(event) {
+  const keyPressed = event.keyCode;
+  const LEFT = 37;
+  const UP = 38;
+  const RIGHT = 39;
+  const DOWN = 40;
+
+  const goingUP = yVelocity == -unitSize;
+  const goingDown = yVelocity == unitSize;
+  const goingRight = xVelocity == unitSize;
+  const goingLeft = xVelocity == -unitSize;
+
+  switch (true) {
+    case keyPressed == LEFT && !goingRight:
+      xVelocity = -unitSize;
+      yVelocity = 0;
+      break;
+    case keyPressed == UP && !goingDown:
+      xVelocity = 0;
+      yVelocity = -unitSize;
+      break;
+    case keyPressed == RIGHT && !goingLeft:
+      xVelocity = unitSize;
+      yVelocity = 0;
+      break;
+    case keyPressed == DOWN && !goingUP:
+      xVelocity = 0;
+      yVelocity = unitSize;
+      break;
+  }
+}
 function checkGameOver() {}
 function displayGameOver() {}
 function resetGame() {}
